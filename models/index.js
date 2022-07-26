0eddee1ac3b8753f2607e2e1f470f1221d4871ec
nrenner <nrenner0211@gmail.com>
2022-07-25 23:03:02 -0400

    post started

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
 // create associations
 // these are common and standard practice
@@ -9,6 +10,18 @@ User.hasMany(Post, {
 
 Post.belongsTo(User, {
     foreignKey: 'user_id'
+});
+
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
 });
 
 module.exports = { User, Post };
